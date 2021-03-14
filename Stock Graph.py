31c9902cb37311e2acfb082550ccf9e9a8674980
Niraj Patel <786nirajpatel@gmail.com>
2021-03-14 06:41:42 -0400

    Added Stock graphing animation

---
+++ Stock Graph.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+APPL_STOCK = pd.read_csv('AAP.csv')
+fig = plt.figure(figsize=(10, 6))
+plt.xlabel('Date',fontsize=20)
+plt.ylabel('Value',fontsize=20)
+plt.title('Apple Stock',fontsize=20)
+
+def animate(i):
+    data = APPL_STOCK.iloc[:i]
+    p = sns.lineplot(x=data.index, y=data['Close'], data=data, color="r")
+    p.tick_params(labelsize=17)
+    plt.setp(p.lines,linewidth=3)
+    return data
+
+ani = matplotlib.animation.FuncAnimation(fig, animate, frames=1000, repeat=False, interval=10)
+plt.show()
