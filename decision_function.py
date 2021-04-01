7c3f79e051121e01a662d370d5b50292d8508ff3
Carson DIckie <carsonmdickie@gmail.com>
2021-04-01 15:05:59 -0400

    Divided main program into main, decision_function, preprocessing
    I made these changes to make the program more modular, more clear separation between tasks (both for the program and for the programmers).
    I also renamed some files so that all adhered to the same naming convention, and deleted files no longer in use.

---
+++ decision_function.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from sklearn.model_selection import KFold, cross_val_predict
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+
+pd.set_option('display.max_rows', None)
+
+	
+def validate_model(data):
+    """
+    validate_model runs the KNN model using 10-fold cross-validation and outputs
+    the accuracy, as well as a confusion matrix and some other performance measures.
+    ----------------------------------------------------------------------------
+    :param data: a dataframe containing stock data
+    :return: None(performance measures are printed)
+    """  
+    global accuracy
+    X = data.iloc[10:, 1: -2]
+    y = data.iloc[10:, -1]
+
+    ### SET UP CROSS-VALIDATION (K-FOLD) ###  
+    k = 10
+    kf = KFold(n_splits=k)
+
+    model = KNN() # SVM-L: linear kernel
+
+    y_pred = cross_val_predict(model, X, y, cv = kf)
+
+    cm = confusion_matrix(y, y_pred)
+    TP = cm[1, 1] # True Positives
+    TN = cm[0, 0] # True Negatives
+    FP = cm[0, 1] # False Positives
+    FN = cm[1, 0] # False Negatives
+    n = len(X)   # Number of Samples
+    # Positive Predicted Value
+    PPV = TP / (TP + FP)
+
+    # Negative Predicted Value
+    NPV = TN / (TN + FN)
+
+    # Specificity
+    specificity = TN / (TN + FP)
+
+    # Sensitivity
+    sensitivity = TP / (TP + FN)
+
+    # Accuracy
+    accuracy = (TP + TN) / n
+	
+    if accuracy < 0.5500:
+       model = KNN() # Defaults to 5 neighbors, Euclidean distance
+	   
+    #print("Confusion Matrix:")
+    print(cm)
+    print("PPV = {:.4f}".format(PPV))
+    print("NPV = {:.4f}".format(NPV))
+    print("Specificity = {:.4f}".format(specificity))
+    print("Sensitivity = {:.4f}".format(sensitivity))
+    print("Accuracy = {:.4f}".format(accuracy))
+	
+def get_accuracy ():
+
+    return accuracy
+    
+def train_model(data):
+    """
+    train_model trains the model using all of the data in the dataframe input. 
+    It returns a trained model that make predictions using model.predict(X).
+    ----------------------------------------------------------------------------
+    :param data: a dataframe containing stock data for training the model
+    :return: a trained model that can be used to make predictions
+    """       
+    X = data.iloc[10:, 1: -2]
+    y = data.iloc[10:, -1]
+    model = SVC()
+    accuracy = get_accuracy ()
+    if accuracy < 0.5500:
+       model = KNN() # Defaults to 5 neighbors, Euclidean distance
+    model.fit(X, y)
+    return model
+  
---
+++ main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from extracting_data import extract
+from preprocessing import preprocess_data
+from stock_graph import graph
+from decision_function import validate_model, train_model
+
+def csv_to_df(csv):
+    """
+    csv_to_df reads a .csv file and converts it into a pandas dataframe
+    ----------------------------------------------------------------------------
+    :param csv: the name of the csv
+    :return: a pandas dataframe containing the information from the csv file
+    """     
+    extract(csv)
+    file_path = csv + ".csv"
+    return pd.read_csv(file_path)
+
+## MAIN / TEST PROGRAM
+# Load the data
+ticker = input("Enter a ticker symbol: ")
+data = csv_to_df(ticker)
+
+# Fix the data
+preprocess_data(data)
+
+# Test the model
+validate_model(data)
+
+# Store the model for making predictions
+knn_model = train_model(data)
+
+# Make a prediction
+X = data.iloc[0:, 1: -2]
+# Can pass a row of a pandas dataframe directly
+predictions = knn_model.predict(X)
+print(predictions)
+graph(data, predictions, ticker)
+# Or construct one using a numpy array and transforming it
+# X2 = np.array([2517, 231.6, 41872770, 229.517, 233.27, 226.46]).reshape(1, -1)
+# predictions2 = knn_model.predict(X2)
+# Output results
+# print(predictions)
+# print(predictions2)
